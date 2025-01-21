9b37942b67aabd702890a47fd7a6f9862aaf9224
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-01-21 11:43:43 +0800

    added db connection

--- __init__.py
+++ __init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 app = Flask(__name__)
 db = SQLAlchemy()
 
-app.config['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+db.init_app(app)
