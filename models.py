2106c8b9f6b6ab8ede29e91abd04ee53307fa086
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-01-21 23:09:34 +0800

    finished the user model and fund model

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from . import db
 
-class User(db.Model):
+class Users(db.Model):
     __tableName__ = "Users"
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(100), nullable=False)
@@ -8,6 +8,22 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), unique=True, nullable=False)
     created_at= db.Column(db.DateTime(timezone=True), server_Default=func.now())
+    funds = db.relationship("Funds", backref="Users")
 
     def __repr__(self):
         return f'<User {self.firstName} {self.id}'
+    
+class Funds(db.Model):
+    __tableName__ = "Funds"
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Numeric(10,2))
+    userId = db.Column(db.Integer, db.ForeignKey("Users.id"))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "amount": self.amount,
+            "created_at": self.created_at,
+        }
